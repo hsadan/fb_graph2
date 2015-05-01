f62da803b390ae1db6c7ce44b9077e12387bcbe1
hsadan <dan@oneuphero.com>
2015-05-01 11:04:53 +0800

    Update test_users.rb

--- lib/fb_graph2/edge/test_users.rb
+++ lib/fb_graph2/edge/test_users.rb
@@ -7,6 +7,11 @@ module FbGraph2
           User.new(user[:id], user)
         end
       end
+      
+      def test_users!(params = {})
+        user = self.post params, edge: :accounts, edge_scope: :'test-users'
+        User.new(user[:id], params.merge(user))
+      end
     end
   end
 end
